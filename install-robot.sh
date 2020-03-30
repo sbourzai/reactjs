e060a44dc008c66f583f5d9accbf047d77417a43
sbourzai <sbourzaim@gmail.com>
2020-03-30 22:35:31 +0200

    Update install-robot.sh

--- install-robot.sh
+++ install-robot.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # comments here :
-echo "strat installing Robot env ..."
-pip install -y robotframework-python3
-pip install -y robotframework 
-pip install --upgrade robotframework-seleniumlibrary
-pip install -y webdrivermanager
+sudo echo "strat installing Robot env ..."
+sudo pip install -y robotframework-python3
+sudo pip install -y robotframework 
+sudo pip install --upgrade robotframework-seleniumlibrary
+sudo pip install -y webdrivermanager
 #1
 cd /tmp/
 wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 #2
-mv chromedriver /usr/bin/chromedriver
+sudo mv chromedriver /usr/bin/chromedriver
 #3
 rpm -qf /usr/lib/libX11.so.6
 sudo yum install -y libX11
 #4
 curl https://intolli.com/install-google-chrome.sh | bash
-mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
 #5
 google-chrome --version && which google-chrome
 #6
-pip install -y selenium
+sudo pip install -y selenium
 robot --version
