2bbfd2dac29887656a57ac0d97795dcec3c77a43
sbourzai <sbourzaim@gmail.com>
2020-03-31 14:53:00 +0200

    Update install-robot.sh

--- install-robot.sh
+++ install-robot.sh
@@ -8,9 +8,6 @@ pip install webdrivermanager --user
 wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 
-
-yum install -y libX11
-
 curl https://intoli.com/install-google-chrome.sh
 
 google-chrome --version && which google-chrome
