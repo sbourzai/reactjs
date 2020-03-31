fb8c0c38195d16b14aa1003fa5fa0180e6f4e754
sbourzai <sbourzaim@gmail.com>
2020-03-31 15:31:43 +0200

    Update install-robot.sh

--- install-robot.sh
+++ install-robot.sh
@@ -2,12 +2,12 @@
 echo "strat installing Robot env ..."
 #pip install robotframework-python3 --user
 
-pip install --upgrade pip --user
-pip install robotframework --user
-pip install robotframework-seleniumlibrary --user
+python -m pip install --upgrade pip --user
+python -m pip install robotframework --user
+python -m pip install robotframework-seleniumlibrary --user
 
-pip install webdriver-manager --user
-pip install chromedriver-binary --user
+python -m pip install webdriver-manager --user
+python -m pip install chromedriver-binary --user
 
-pip install selenium --user
+python -m pip install selenium --user
 robot --version
