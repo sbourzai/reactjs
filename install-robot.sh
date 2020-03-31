5def4b6a43887328fb9eee79cbc8cebb28d5a291
sbourzai <sbourzaim@gmail.com>
2020-03-31 14:15:06 +0200

    Update install-robot.sh

--- install-robot.sh
+++ install-robot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "strat installing Robot env ..."
-pip install  robotframework-python3 --admin
-pip install  robotframework --admin
-pip install --upgrade robotframework-seleniumlibrary
-pip install webdrivermanager
+pip install  robotframework-python3 --user
+pip install  robotframework --user
+pip install --upgrade robotframework-seleniumlibrary --user
+pip install webdrivermanager --user
 
 wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
@@ -15,5 +15,5 @@ curl https://intoli.com/install-google-chrome.sh
 
 google-chrome --version && which google-chrome
 
-pip install selenium
+pip install selenium --user
 robot --version
