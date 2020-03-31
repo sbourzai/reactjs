cce50fdabbbba6b6453141eeb5f425ed13b09e3b
sbourzai <sbourzaim@gmail.com>
2020-03-31 20:42:17 +0200

    Update install-robot.sh

--- install-robot.sh
+++ install-robot.sh
@@ -7,6 +7,10 @@ python -m pip install robotframework --user
 python -m pip install robotframework-seleniumlibrary --user
 
 python -m pip install webdriver-manager --user
+
+wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+
 python -m pip install chromedriver-binary --user
 
 #python -m pip install selenium --user
