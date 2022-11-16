75fd83b4096b5edadd14a7925126905f1f55142c
sbourzai <sbourzaim@gmail.com>
2022-11-16 15:59:21 +0100

    Update and rename new_stagelibraries.groovy to src/sayHello.groovy

--- new_stagelibraries.groovy
+++
@@ -1,9 +0,0 @@
-#!groovy
-// Write or add Functions(definations of stages) which will be called from your jenkins file
-def MyNew_library()
- {
-     echo "Function execution of MyNew_library"
-     // You can add yoiur functionality here
- }
-
-return this
